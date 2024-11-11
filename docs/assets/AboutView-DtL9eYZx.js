f54edb91e422847f65ef985dcdcece6d58897e5d
DavidDuveau <david.duveau@hotmail.fr>
2024-11-11 22:39:54 +0100

    'deploy'

--- docs/assets/AboutView-DtL9eYZx.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD:docs/assets/AboutView-CzP9JR7-.js
-import{_ as o,c as s,a as t,o as a}from"./index-CDjzZrK7.js";const n={},c={class:"about"};function r(_,e){return a(),s("div",c,e[0]||(e[0]=[t("h1",null,"This is an about page",-1)]))}const l=o(n,[["render",r]]);export{l as default};
-=======
-import{_ as o,c as s,a as t,o as a}from"./index-DvtH3YkP.js";const n={},c={class:"about"};function r(_,e){return a(),s("div",c,e[0]||(e[0]=[t("h1",null,"This is an about page",-1)]))}const l=o(n,[["render",r]]);export{l as default};
->>>>>>> 257b39210811bc200c35a1d4e67a3bfcd9fb6929:docs/assets/AboutView-DtL9eYZx.js
